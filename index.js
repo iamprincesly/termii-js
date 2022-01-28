1dbc2d76501fcd4ac4e8a95a127ae224bebb1cb9
iamprincesly <iamprincesly@gmail.com>
2022-01-28 02:20:26 +0100

    Add entry script

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * ----------------------------------------------------------------
+ * This file is part of the Termii JS SDK.
+ * A Node.js wrapper made for Termii API. 
+ * For more information, visit: https://developer.termii.com
+ * 
+ * (c) Sylvanus Etim <iamprincesly@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * ----------------------------------------------------------------
+ */
+
+/**
+ * Export Termii main class
+ */
+module.exports = require('./lib/Termii');
